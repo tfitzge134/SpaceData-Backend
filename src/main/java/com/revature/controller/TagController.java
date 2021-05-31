619fc20105a93fbdfb0a35b32fcb6297917b65f0
Teresa <tfitzge134@gmail.com>
2021-05-31 16:47:15 -0500

    added cors

--- src/main/java/com/revature/controller/TagController.java
+++ src/main/java/com/revature/controller/TagController.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.revature.service.TagService;
  * @author teresafitzgerald
  *
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/tag")
 public class TagController {
